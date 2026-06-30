e7ad7fc96456eb15376018adbb2474ba8cd9da49
akshad110 <codewithaksush01@gmail.com>
2026-06-30 16:04:28 +0530

    made some changes in stage 7

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,5 +1,26 @@
-export function fetchNotifications() {
-    let api =  await = fetch('http://4.226.186.213/evaluation-service/notifications');
+const axios  = require('axios');
+require('dotenv').config();
+
+const BASE_URL = "http://4.224.186.213/evaluation-service/notifications";
+
+export async function fetchNotifications(page = 1, filter = "All") {
+  const token = process.env.TOKEN; // Put your access token here
+
+  const params = {
+    page,
+    limit: 10,
+  };
+
+  if (filter !== "All") {
+    params.notification_type = filter;
+  }
+
+  const response = await axios.get(BASE_URL, {
+    params,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
 }
-
-
